f2d156cce4e949b530c72a7b7927700034f89750
slimylove <shenglimylove@gmail.com>
2021-03-20 20:20:12 +0800

    测试git push 23

--- t.sh
+++ t.sh
@@ -3,3 +3,10 @@ echo ${PLUGIN_SSH_KEY}
 echo ${PLUGIN_SSH_KEY} | wc -l
 echo ${GIT_PUSH_SSH_KEY}
 echo ${GIT_PUSH_SSH_KEY} | wc -l
+
+
+echo "================================"
+echo -ne ${PLUGIN_SSH_KEY}
+echo -ne ${PLUGIN_SSH_KEY} | wc -l
+echo -ne ${GIT_PUSH_SSH_KEY}
+echo -ne ${GIT_PUSH_SSH_KEY} | wc -l
